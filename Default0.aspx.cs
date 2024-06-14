fcc7c3c7156974bb714cc76c46fe10631ad48520
Honjuu <120557643+Honjuu@users.noreply.github.com>
2024-06-14 23:33:42 +0300

    Update Default0.aspx.cs

--- Default0.aspx.cs
+++ Default0.aspx.cs
@@ -1,14 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class projects_1_Default : System.Web.UI.Page
+public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Label1.Text = Context.Request.UserHostAddress;
+        if (Request.HttpMethod == "GET")
+        {
+            Response.ContentType = "text/plain";
+            Response.Write("Hello from the server! This content is loaded dynamically via AJAX.");
+            Response.End();
+        }
     }
 }
